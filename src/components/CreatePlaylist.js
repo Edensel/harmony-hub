3dc9c18139c01b657c7910b56fc47e67117f1393
Ashley <ashley.precios@student.moringaschool.com>
2023-12-15 00:35:23 +0300

    Inputing the local storage operations

--- src/components/CreatePlaylist.js
+++ src/components/CreatePlaylist.js
@@ -4,6 +4,11 @@ function CreatePlaylist() {
     const [playlistName, setPlaylistName] = useState("");
 
     const create = () => {
+    let currentPlaylist = localStorage.getItem("allPlaylist");
+   
+    currentPlaylist = JSON.parse(currentPlaylist);
+    currentPlaylist[playlistName] = []; 
+    localStorage.setItem("allPlaylist", currentPlaylist.toString());
 
     };
 }

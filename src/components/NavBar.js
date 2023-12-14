5ec19f989510e1c264e9431ab46eba39a5817bbf
DenselEsekon <densel.esekon@moringaschool.com>
2023-12-15 01:54:41 +0300

    Created the navigation bar layout using Bootstrap classes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,66 @@
 import React from "react";
 
 const NavBar = ({ keyword, handleKeyPress, setKeyword, fetchMusicData }) => {
+
   return (
     
+    <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/v-music">
+          <img src="logo.png" alt="Harmony Hub" /> Harmony-Hub
+        </a>
+        <div className="dropdown">
+          <button
+            className="btn btn-sm btn-secondary dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Liked Music
+          </button>
+          <ul className="dropdown-menu">
+            <li>
+              <a className="dropdown-item text-secondary" href="/">
+                All Playlist
+              </a>
+            </li>
+            <li>
+              <Link className="dropdown-item" to="/likedMusic">
+                Liked Music
+              </Link>
+            </li>
+            <li>
+              <button
+                type="button"
+                className="btn text-secondary"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+              >
+                Create new
+              </button>
+            </li>
+          </ul>
+        </div>
+
+        <div
+          className="collapse navbar-collapse d-flex justify-content-center"
+          id="navbarSupportedContent"
+        >
+          <input
+            value={keyword}
+            onChange={(event) => setKeyword(event.target.value)}
+            onKeyDown={handleKeyPress}
+            className="form-control me-2 w-75"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button onClick={fetchMusicData} className="btn btn-outline-success">
+            Search
+          </button>
+        </div>
+      </div>
+    </nav>
   );
 };
 

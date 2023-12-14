2bcaf938bc46d38839cd5b8e01c115a0f4dd43ad
DenselEsekon <densel.esekon@moringaschool.com>
2023-12-15 01:31:47 +0300

    Created React Router Routes for handling different pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-import NavBar from './components/NavBar';
+import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   const [keyword, setKeyword] = useState("");
@@ -8,67 +8,65 @@ function App() {
   const [tracks, setTracks] = useState([]);
   const [token, setToken] = useState(null);
   // First we create a function to fetch music data from the Spotify API based on search query.
-      
 
-    const fetchMusicData = async () => {
-      setIsLoading(true);
-      try {
-        const response = await fetch(
-          `https://api.spotify.com/v1/search?q=${keyword}&type=track`, 
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-  
-        if (!response.ok) {
-          throw new Error("Failed to fetch music data");
+  const fetchMusicData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(
+        `https://api.spotify.com/v1/search?q=${keyword}&type=track`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         }
-  
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch music data");
+      }
+
+      const jsonData = await response.json();
+      setTracks(jsonData.tracks.items);
+    } catch (error) {
+      setMessage(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      fetchMusicData();
+    }
+  };
+
+  useEffect(() => {
+    initializePlaylist();
+
+    const fetchToken = async () => {
+      try {
+        const response = await fetch("https://accounts.spotify.com/api/token", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          body: "grant_type=client_credentials&client_id=3cf148b50f394ed7bf135002bea69400&client_secret=71cf1d5bce2b45b0a8f5cd9e08e75556",
+        });
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch token");
+        }
+
         const jsonData = await response.json();
-        setTracks(jsonData.tracks.items);
+        setToken(jsonData.access_token);
       } catch (error) {
         setMessage(error.message);
       } finally {
         setIsLoading(false);
       }
     };
-  
-    const handleKeyPress = (event) => {
-      if (event.key === "Enter") {
-        fetchMusicData();
-      }
-    };
-    
-    useEffect(() => {
-      initializePlaylist();
-  
-      const fetchToken = async () => {
-        try {
-          const response = await fetch("https://accounts.spotify.com/api/token", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/x-www-form-urlencoded",
-            },
-            body: "grant_type=client_credentials&client_id=3cf148b50f394ed7bf135002bea69400&client_secret=71cf1d5bce2b45b0a8f5cd9e08e75556",
-          });
-  
-          if (!response.ok) {
-            throw new Error("Failed to fetch token");
-          }
-  
-          const jsonData = await response.json();
-          setToken(jsonData.access_token);
-        } catch (error) {
-          setMessage(error.message);
-        } finally {
-          setIsLoading(false);
-        }
-      };
-      fetchToken();
-    }, []);
-
+    fetchToken();
+  }, []);
 
   return (
     <>
@@ -78,6 +76,13 @@ function App() {
         handleKeyPress={handleKeyPress}
         fetchMusicData={fetchMusicData}
       />
+
+      <Routes>
+        <Route path="/likedMusic" element={<LikedMusic />} />
+        <Route path="/" element={<div></div>} />
+      </Routes>
+
+      
     </>
   );
 }

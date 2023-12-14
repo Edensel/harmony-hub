8d37f7ad5ea3372aad6afe7f836d5072290e3111
DenselEsekon <densel.esekon@moringaschool.com>
2023-12-15 01:23:07 +0300

    Added a function for fetching data from the Spotify API

--- src/App.js
+++ src/App.js
@@ -2,10 +2,50 @@ import './App.css';
 import NavBar from './components/NavBar';
 
 function App() {
+  // First we create a function to fetch music data from the Spotify API based on search query.
+  
+    const fetchMusicData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(
+          `https://api.spotify.com/v1/search?q=${keyword}&type=track`, 
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+  
+        if (!response.ok) {
+          throw new Error("Failed to fetch music data");
+        }
+  
+        const jsonData = await response.json();
+        setTracks(jsonData.tracks.items);
+      } catch (error) {
+        setMessage(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+  
+    const handleKeyPress = (event) => {
+      if (event.key === "Enter") {
+        fetchMusicData();
+      }
+    };
+    
+
+
   return (
-    <div>
-      < NavBar />
-    </div>
+    <>
+      <Navbar
+        keyword={keyword}
+        setKeyword={setKeyword}
+        handleKeyPress={handleKeyPress}
+        fetchMusicData={fetchMusicData}
+      />
+    </>
   );
 }
 

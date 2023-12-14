5cf09bca335617ca1183a1ac8b01537be6ab0d50
Yelsha <144707114+Ashleyprecious@users.noreply.github.com>
2023-12-15 00:57:31 +0300

    Revert "Update and correction of the code"

--- src/components/CreatePlaylist.js
+++ src/components/CreatePlaylist.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 function CreatePlaylist() {
     const [playlistName, setPlaylistName] = useState("");
@@ -9,46 +9,36 @@ function CreatePlaylist() {
     currentPlaylist = JSON.parse(currentPlaylist);
     currentPlaylist[playlistName] = []; 
     localStorage.setItem("allPlaylist", currentPlaylist.toString());
+
     };
 
     return (
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h1 className="modal-title fs-5" id="exampleModalLabel">
-                New Playlist
-              </h1>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              />
-            </div>
-            <div className="modal-body">
-              <input
-                type="text"
-                placeholder="Enter Name"
-                className="form-control"
-                value={playlistName}
-                onChange={(e) => setPlaylistName(e.target.value)}
-              />
-            </div>
-            <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-bs-dismiss="modal"
-              >
-                Cancel
-              </button>
-              <button onClick={create} type="button" className="btn btn-primary">
-                Create
-              </button>
-            </div>
-          </div>
+        <div className="modal-dialog"></div>
+            {/* .... */}
         </div>
-      );
-    }
+    <div className="modal-body"></div>
+        <input
+          type="text"
+          placeholder="Enter Name"
+          className="form-control"
+          value={playlistName}
+          onChange={(e) => setPlaylistName(e.target.value)}
+        />
+    </div>
+    <div className="modal-footer">
+          <button
+            type="button"
+            className="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            Cancel
+          </button>
+          <button onClick={create} type="button" className="btn btn-primary">
+            Create
+          </button>
+        </div>
+
+    );
+}
 
 export default CreatePlaylist;

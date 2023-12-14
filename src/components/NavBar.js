b9db6e13a943a0e10dec2a8f07aee180f06dad15
DenselEsekon <densel.esekon@moringaschool.com>
2023-12-15 01:52:00 +0300

    Updated NavBar component for displaying the navigation bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,9 @@
 import React from "react";
 
-function NavBar() {
+const NavBar = ({ keyword, handleKeyPress, setKeyword, fetchMusicData }) => {
   return (
-    <div>
-        <h1>Hello Moringa</h1>
-      <h3>Harmony Kenyan</h3>
-      <h3>Harmony Nairobi</h3>
-      <h3>Harmony Hub</h3>
-      <h3>Harmony Hub</h3>
-    </div>
+    
   );
-}
+};
 
 export default NavBar;

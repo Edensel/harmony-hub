87d2ecb5a1bd3f6defdbe10a182fd0d1b852c129
Ashley <ashley.precios@student.moringaschool.com>
2023-12-15 01:21:58 +0300

    Final Commit

--- src/components/CreatePlaylist.js
+++ src/components/CreatePlaylist.js
@@ -13,40 +13,40 @@ function CreatePlaylist() {
 
     return (
         <div className="modal-dialog">
-          <div className="modal-content">
-          <div className="modal-header">
-          <h1 className="modal-title fs-5" id="exampleModalLabel">
-            New Playlist
-          </h1>
-          <button
-            type="button"
-            className="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          />
-        </div>
-        <div className="modal-body">
-        <input
-          type="text"
-          placeholder="Enter Name"
-          className="form-control"
-          value={playlistName}
-          onChange={(e) => setPlaylistName(e.target.value)}
-        />
-    </div>
-    <div className="modal-footer">
-          <button
-            type="button"
-            className="btn btn-secondary"
-            data-bs-dismiss="modal"
-          >
-            Cancel
-          </button>
-          <button onClick={create} type="button" className="btn btn-primary">
-            Create
-          </button>
-        </div>
-        </div>
+            <div className="modal-content">
+                <div className="modal-header">
+                    <h1 className="modal-title fs-5" id="exampleModalLabel">
+                      New Playlist
+                    </h1>
+                    <button
+                       type="button"
+                       className="btn-close"
+                       data-bs-dismiss="modal"
+                       aria-label="Close"
+                    />
+                </div>
+                <div className="modal-body">
+                    <input
+                       type="text"
+                       placeholder="Enter Name"
+                       className="form-control"
+                       value={playlistName}
+                       onChange={(e) => setPlaylistName(e.target.value)}
+                    />
+                </div>
+                <div className="modal-footer">
+                    <button
+                      type="button"
+                      className="btn btn-secondary"
+                      data-bs-dismiss="modal"
+                    >
+                      Cancel
+                    </button>
+                    <button onClick={create} type="button" className="btn btn-primary">
+                      Create
+                    </button>
+                </div>
+            </div>
         </div>
 
     );

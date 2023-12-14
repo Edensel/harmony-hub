079b291ede52b9022ca6b7d3c8cb204066774c90
DenselEsekon <densel.esekon@moringaschool.com>
2023-12-14 22:44:10 +0300

    Last test

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,7 @@ import React from "react";
 function NavBar() {
   return (
     <div>
+        <h1>Hello Moringa</h1>
       <h3>Harmony Hub</h3>
       <h3>Harmony Hub</h3>
       <h3>Harmony Hub</h3>

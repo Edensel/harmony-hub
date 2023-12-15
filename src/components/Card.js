eae0bf3b1748b9f6d79a6e3bcb6cfceff794254e
Martin <mmaina290@gmail.com>
2023-12-15 18:28:42 +0300

    Martin

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-export default function Card() {
+export default function Card({element}) {
     const [likedMusic, setlikedMusic] = useState([]);
 
     const handleLike = () => {
